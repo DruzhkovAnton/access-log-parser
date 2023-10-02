431d0f18157693b5a1150bbf8839d84cd840784c
druzhkov anton <karatevlg@gmail.com>
2023-10-02 23:27:45 +0300

    задание с арифметикой

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int number2 = new Scanner(System.in).nextInt();
+
+        int sum = number1 + number2;
+        System.out.println("Сумма: " + sum);
+        int sum1 = number1 - number2;
+        System.out.println("Разность: " + sum1);
+        int sum2 = number1 * number2;
+        System.out.println("Произведение: " + sum2);
+        double sum3 = (double) number1 / number2;
+        System.out.println("Частность: " + sum3);
     }
 }
