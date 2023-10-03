20480f0047ddb66176e9aea920ec67a332929ad6
druzhkov anton <karatevlg@gmail.com>
2023-10-04 01:28:09 +0300

    public static double fraction(double x);

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        double x = 5.3;
+        System.out.println("fraction: " + fraction(x));
+    }
+
+    public static double fraction(double x){
+        int y=(int)x;
+        return  x-y;
     }
 }
