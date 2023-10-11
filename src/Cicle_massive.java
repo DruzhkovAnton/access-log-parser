2ca108e7f3c4767fab119f52946b7afd5c6ea5ef
druzhkov anton <karatevlg@gmail.com>
2023-10-11 21:41:24 +0300

    public static String listNums(int x);

---
+++ src/Cicle_massive.java
@@ -0,0 +1,11 @@
+public class Cicle_massive {
+
+    public static String listNums(int x){
+       String str = "0";
+        for(int i=1; i<=x; i++){
+            str = str+" "+i;
+        }
+        return str;
+    }
+
+}
