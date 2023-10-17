40ab46aaf028e3d54a5210cf911fb23951228b0f
druzhkov anton <karatevlg@gmail.com>
2023-10-17 21:17:51 +0300

    курсовая по циклам

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        int i = 0;
+        do{
+            System.out.println("Введите путь к файлу и нажмите enter:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            if (file.exists() && file.isFile()){
+                i++;
+                System.out.println(path + " - Путь указан верно "+i+" раз");
+                continue;
+            }if (file.isDirectory()){
+                System.out.println(path + " - не является путем к файлу, это папка, укажите полный путь к файлу");
+            }else {
+                System.out.println(path + " - не является путем к файлу или папке");
+            }
+        }while (1==1);
     }
 }
