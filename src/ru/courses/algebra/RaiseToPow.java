7e2356afe051e94ac713de91653d532d63eb9670
druzhkov anton <karatevlg@gmail.com>
2023-11-16 21:10:47 +0300

    Пакеты 3, еще одно исправление

--- src/ru/courses/algebra/RaiseToPow.java
+++ src/ru/courses/algebra/RaiseToPow.java
@@ -1,11 +1,11 @@
 package ru.courses.algebra;
 
 public class RaiseToPow {
-    public static String toPow(String b, String y){
+    public static void toPow(String b, String y){
         int a =Integer.parseInt(b);
         int x = Integer.parseInt(y);
-        int res = 0;
-        res = (int) Math.pow(a,x);
+        double res = 0;
+        res =  Math.pow(a,x);
         System.out.println(res);
     }
 }
