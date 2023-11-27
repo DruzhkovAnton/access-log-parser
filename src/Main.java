ba4558f95359b6eb4b6b3508b853751b849348ab
druzhkov anton <karatevlg@gmail.com>
2023-11-28 00:52:54 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,23 @@
-import ru.courses.parser.ParserLog;
+import java.util.ArrayList;
 
-import java.io.*;
-import java.text.ParseException;
-import java.util.Scanner;
-
-import static ru.courses.parser.ParserLog.parse;
-
-// C:\Users\admin\Downloads\access (1).log
 public class Main {
-    public static void main(String[] args) throws IOException {
-        do {
-            System.out.println("Введите путь к файлу и нажмите enter:");
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            if (file.exists() && file.isFile()){
-                System.out.println(path + " - Путь указан верно");
-                parse(path);
-                continue;
-            }if (file.isDirectory()){
-                System.out.println(path + " - не является путем к файлу, это папка, укажите полный путь к файлу");
-            }else {
-                System.out.println(path + " - не является путем к файлу или папке");
-            }
-        } while (1 == 1);
+    public static void main(String[] args)  {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = 10;
+
+        for (int i=0;i<n;i++){
+            list.add(i+1);
+        }
+        System.out.println(list);
+        System.out.println("  ");
+
+        for (int i=0;i<n;i+=2){
+            int temp = list.get(i);
+            list.set(i,list.get(i+1));
+            list.set(i+1, temp);
+        }
+        System.out.println(list);
+
     }
 
 }
