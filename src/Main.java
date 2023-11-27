f31d117bed119184c699265d14e3236369deb4d6
druzhkov anton <karatevlg@gmail.com>
2023-11-28 01:12:06 +0300

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import static ru.courses.parser.BubleSort.bubbleSort;
 
 public class Main {
     public static void main(String[] args)  {
@@ -16,7 +20,16 @@ public class Main {
             list.set(i,list.get(i+1));
             list.set(i+1, temp);
         }
+
+        int[] list1 = {2, 1, 4, 3, 6, 5, 8, 7, 10, 9};
         System.out.println(list);
+        bubbleSort(list);
+        bubbleSort(list1);
+        System.out.println(list);
+        System.out.println(Arrays.toString(list1));
+
+
+
 
     }
 
---
+++ src/ru/courses/parser/BubleSort.java
@@ -0,0 +1,31 @@
+package ru.courses.parser;
+
+import java.util.ArrayList;
+
+public class BubleSort {
+
+    public static void bubbleSort(ArrayList<Integer> intList) {
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public static void bubbleSort(int[] intArray) {
+        int n = intArray.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
