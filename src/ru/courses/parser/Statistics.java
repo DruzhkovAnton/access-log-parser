6516235f42a4f209d6570f8ff5653dcf84b6c0a4
druzhkov anton <karatevlg@gmail.com>
2023-12-10 02:28:19 +0300

    задание 1 - Collections - курсач

--- src/ru/courses/parser/Statistics.java
+++ src/ru/courses/parser/Statistics.java
@@ -2,12 +2,17 @@ package ru.courses.parser;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class Statistics {
     static int totalTrafic;
     static LocalDateTime minTime;
     static LocalDateTime maxTime;
     static Duration duration;
+    static  HashSet<String> saitList;
+    static HashMap<String, Integer> staOS;
     public Statistics(){
         this.totalTrafic = 0;
         this.minTime = LocalDateTime.now();
@@ -25,11 +30,39 @@ public class Statistics {
         if (maxTime.isBefore(log.getDateTime())){
             maxTime = log.getDateTime();
         }
+
+        if(log.getHttpResponseCode()==200){
+            saitList.add(log.getResourcePath());
+        }
+
+        UserAgent os = new UserAgent(log.getUserAgent());
+
+        if(staOS.containsKey(os.getOperatingSystem())){
+            staOS.put(os.getOperatingSystem(),staOS.get(os.getOperatingSystem())+1);
+        }else {
+            staOS.put(os.getOperatingSystem(),1);
+        }
     }
+
+    public static HashMap<String, Double> calcStatOS(){
+        HashMap<String, Double> allstatOS = new HashMap<>();
+        int totalOS = staOS.values().stream().mapToInt(Integer::intValue).sum();
+
+        for (Map.Entry<String, Integer> entry : staOS.entrySet()){
+            double calc = (double) entry.getValue()/totalOS;
+            allstatOS.put(entry.getKey(), calc);
+        }
+        return allstatOS;
+    }
+
 
     public static int getTrafficRate(){
         duration = Duration.between(minTime, maxTime);
         return totalTrafic/(int) duration.toHours();
+    }
+
+    public static HashSet<String> getSaitList(){
+        return saitList;
     }
 
 
