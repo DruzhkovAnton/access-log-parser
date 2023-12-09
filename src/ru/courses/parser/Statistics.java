8ef10d18075cf6d046d42ab2155b62f8df0bd06f
druzhkov anton <karatevlg@gmail.com>
2023-12-10 02:48:12 +0300

    задание 2 - Collections - курсач

--- src/ru/courses/parser/Statistics.java
+++ src/ru/courses/parser/Statistics.java
@@ -12,7 +12,9 @@ public class Statistics {
     static LocalDateTime maxTime;
     static Duration duration;
     static  HashSet<String> saitList;
+    static  HashSet<String> notExistSaitList;
     static HashMap<String, Integer> staOS;
+    static HashMap<String, Integer> staBrowser;
     public Statistics(){
         this.totalTrafic = 0;
         this.minTime = LocalDateTime.now();
@@ -35,12 +37,24 @@ public class Statistics {
             saitList.add(log.getResourcePath());
         }
 
+        if(log.getHttpResponseCode()==404) {
+            notExistSaitList.add(log.getResourcePath());
+        }
+
         UserAgent os = new UserAgent(log.getUserAgent());
+
+
 
         if(staOS.containsKey(os.getOperatingSystem())){
             staOS.put(os.getOperatingSystem(),staOS.get(os.getOperatingSystem())+1);
         }else {
             staOS.put(os.getOperatingSystem(),1);
+        }
+
+        if(staBrowser.containsKey(os.getBrowser())){
+            staBrowser.put(os.getOperatingSystem(),staBrowser.get(os.getOperatingSystem())+1);
+        }else {
+            staBrowser.put(os.getOperatingSystem(),1);
         }
     }
 
@@ -55,6 +69,17 @@ public class Statistics {
         return allstatOS;
     }
 
+    public static HashMap<String, Double> calcStatBrowser(){
+        HashMap<String, Double> allstatBrowser = new HashMap<>();
+        int totalBrowser = staBrowser.values().stream().mapToInt(Integer::intValue).sum();
+
+        for (Map.Entry<String, Integer> entry : staBrowser.entrySet()){
+            double calc = (double) entry.getValue()/totalBrowser;
+            allstatBrowser.put(entry.getKey(), calc);
+        }
+        return allstatBrowser;
+    }
+
 
     public static int getTrafficRate(){
         duration = Duration.between(minTime, maxTime);
@@ -63,6 +88,10 @@ public class Statistics {
 
     public static HashSet<String> getSaitList(){
         return saitList;
+    }
+
+    public static HashSet<String> getNotExistSaitList(){
+        return notExistSaitList;
     }
 
 
